3312ac8cdacf796244bdbf92857338463e28ee03
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-12-27 15:59:57 -0800

    Added portfolio items via Resource generator

---
+++ app/controllers/protfolios_controller.rb
@@ -0,0 +1,2 @@
+class ProtfoliosController < ApplicationController
+end
---
+++ db/migrate/20181227235530_create_protfolios.rb
@@ -0,0 +1,13 @@
+class CreateProtfolios < ActiveRecord::Migration[5.2]
+  def change
+    create_table :protfolios do |t|
+      t.string :title
+      t.string :subtitle
+      t.text :body
+      t.text :main_image
+      t.text :thumb_image
+
+      t.timestamps
+    end
+  end
+end
