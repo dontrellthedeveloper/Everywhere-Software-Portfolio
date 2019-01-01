b1d474e5603f9a0fc479d22f389e2a4fefe6e97a
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-12-31 19:33:28 -0800

    Integrated new gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = EverywhereSoftwareTool::Renderer.copyright 'Everywhere Software', 'All rights reserved'
+  end
+end
+
+
+module EverywhereSoftwareTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b> #{name} </b> #{msg}".html_safe
+    end
+  end
 end
