9494a6080d1888be75039d61c290f00c6e7bebc9
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-01-12 04:16:09 -0800

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -50,6 +50,21 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+       alert_generator alert
+    end
+  end
+
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Everywhere Software", sticky: false, time: 3000)
+  end
+
 end
 
 
