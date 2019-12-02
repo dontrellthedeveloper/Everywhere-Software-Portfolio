0d6891ae6fd0c2a8f6ddc1182aee42c704189583
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-12-02 01:54:33 -0800

    Changed name of blog from Program-to-Pro Dev -> Everywhere Software Blog

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,11 +34,11 @@ module ApplicationHelper
       {
           url: portfolios_path,
           title: 'Portfolio'
-      },
-      {
-          url: tech_news_path,
-          title: 'Tech News'
       }
+      #{
+      #    url: tech_news_path,
+      #    title: 'Tech News'
+      #}
   ]
   end
 
